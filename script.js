0121dd5a734425c78da16f64089eb8c98cc6d607
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-12-24 23:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-// Complete the js code
-function Car(make, model) {}
+// // Complete the js code
+// function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+// function SportsCar(make, model, topSpeed) {}
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+// // Do not change the code below
+// window.Car = Car;
+// window.SportsCar = SportsCar;
+
+
+function car(make, model){
+this.make=make;
+this.model=model;
+}
+
+car.prototype.getmakemodel=function(){
+    return `${this.make}  ${this.model}`;
+}
+
+
+// Define the SportsCar Constructor
+function SportsCar(make,model,topspeed){
+    car.call(this,make,model);
+    this.topspeed=topspeed;
+}
+// Set Up Inheritance
+SportsCar.prototype =Object.create(car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+
+
+
+SportsCar.prototype.gettopspeed =function(){
+    return this.topspeed;
+}
